b364c613002c79fcd9878a51f35495dbaa53ded5
Maddie Crothers <maddiecrothers@gmail.com>
2013-11-27 20:10:26 -0500

    push to heroku

--- Gemfile
+++ Gemfile
@@ -2,10 +2,18 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+group :deveopment do
+	gem 'sqlite3'
+end
+
+group :production do
+	gem 'pg'
+end
+
 
 
 # Gems used only for assets and not required
@@ -21,6 +29,9 @@ group :assets do
 end
 
 gem 'jquery-rails'
+
+gem 'twitter-bootstrap-rails'
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,4 +1,8 @@
 class GiftsController < ApplicationController
+  
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
   def index
   	@gifts = Gift.all 
   end
@@ -6,4 +10,25 @@ class GiftsController < ApplicationController
   def new
   	@gifts = Gift.new 
   end
+
+def create
+	@gift = Gift.new (params[:gift])
+
+	if @gift.save
+		# #create twitter API client
+  #     client = Twitter::REST::Client.new do |config|
+  #       config.consumer_key        = "WjVbbY5SpPGg1LLJsZq46A"
+  #       config.consumer_secret     = "mArP7ZqRIz2vPM0QHrqKciqpgsUvAgUODRR9RaHMQ"
+  #       config.access_token        = "2205349028-0UH8agMmHdEuKu5DPZxSYqwIWUpU1zzGzsu6hxY"
+  #       config.access_token_secret = "ISlTvu59p7SdRAxawSBcZqMmt0vcMSlXPAR07gHGGUQeq"
+  #     end
+
+  #     client.update("@mjrcrothers gets a latte on me! #mdm #{gift.from}")
+
+		client.update(@mjrcrothers have a latte on me! #{@gift.from} from: #{@gift.from})
+    flash[:info] = "thanks a-latte!"
+		redirect_to :back
+	end
+end
+
 end
